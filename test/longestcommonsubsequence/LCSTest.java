176fc392afc58f7da11b6f0000e0aa455770e6b4
Natanael Martinez Morales <natanael29032000@gmail.com>
2021-01-11 19:59:55 +0000

    LongestCommonSubsequence

--- test/longestcommonsubsequence/LCSTest.java
+++ test/longestcommonsubsequence/LCSTest.java
@@ -8,6 +8,9 @@ public class LCSTest {
     private String [] sequences;
     private String result1;
     
+    private String[] sequences1;
+    private String result2;
+    
     public LCSTest() {
     }
 
@@ -17,11 +20,16 @@ public class LCSTest {
         sequences[0] = "AGGTAB";
         sequences[1] = "GXTXAYB";
         result1 = "BATG";
+        
+        sequences1 = new String[2];
+        sequences1[0] = "ABCDGH";
+        sequences1[1] = "AEDFHR";
+        result2 = "HDA";
     }
     
     @Test
-    public void testTabulation() {
-        System.out.println("Test: Tabulation");
+    public void testTabulation1() {
+        System.out.println("Test: Tabulation 1");
         
         String result = SolveTabulation.solveTabulation(sequences, sequences[0].length(), sequences[1].length());
         
@@ -29,11 +37,29 @@ public class LCSTest {
     }
     
     @Test
-    public void testMemoization() {
-        System.out.println("Test: Memoization");
+    public void testTabulation2() {
+        System.out.println("Test: Tabulation 2");
+        
+        String result = SolveTabulation.solveTabulation(sequences1, sequences1[0].length(), sequences1[1].length());
+        
+        assertEquals("Resultado incorrecto: Las cadenas no coinciden", result2, result);
+    }
+    
+    @Test
+    public void testMemoization1() {
+        System.out.println("Test: Memoization 1");
         
         String result = SolveMemoization.solveMemoization(sequences, sequences[0].length(), sequences[1].length());
         
         assertEquals("Resultado incorrecto: Las cadenas no coinciden", result1, result);
+    }
+    
+    @Test
+    public void testMemoization2() {
+        System.out.println("Test: Memoization 2");
+        
+        String result = SolveMemoization.solveMemoization(sequences1, sequences1[0].length(), sequences1[1].length());
+        
+        assertEquals("Resultado incorrecto: Las cadenas no coinciden", result2, result);
     }
 }
